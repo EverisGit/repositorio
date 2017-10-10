34983501d4f228d3db199ca17a6488daf4631ff3
aribeira <alexsander.ribeiro.avila@everis.com>
2017-10-10 16:57:18 -0300

    Primeiro commit

---
+++ ProjetoJava/src/br/com/everis/git/Modulo1.java
@@ -0,0 +1,10 @@
+package br.com.everis.git;
+
+public class Modulo1 {
+
+	
+	
+	public String menssagem(){
+		return "Modulo1";
+	}
+}
---
+++ ProjetoJava/src/br/com/everis/git/Principal.java
@@ -0,0 +1,15 @@
+package br.com.everis.git;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("#Inicio");
+
+		System.out.println(new Modulo1().menssagem());
+		
+		System.out.println("#FIM");
+
+	}
+
+}
